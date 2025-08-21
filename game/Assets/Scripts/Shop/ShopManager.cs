7646088e4f2a266d043a35274c814cbc5755145c
owais-star <iqbalowais35@gmail.com>
2025-08-21 10:28:15 +0500

    WIP

--- game/Assets/Scripts/Shop/ShopManager.cs
+++ game/Assets/Scripts/Shop/ShopManager.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+// using UnityWebView;  // Ensure you are using UnityWebView
 
 [Serializable]
 public class UIDataConatainer
@@ -17,16 +18,31 @@ public class UIDataConatainer
     public Image boosterImage;
 }
 
-
 public class ShopManager : MonoBehaviour
 {
     public static ShopManager Instance;
+    private WebViewObject webViewObject;  // Declare the WebViewObject
+
     void Awake()
     {
-        if(Instance == null)
+        if (Instance == null)
         {
             Instance = this;
         }
+    }
+
+    void Start()
+    {
+        // Initialize the WebViewObject
+        webViewObject = new GameObject("WebViewObject").AddComponent<WebViewObject>();
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+        webViewObject.canvas = GameObject.Find("Canvas");
+        if (webViewObject.canvas == null)
+        {
+            Debug.LogError("Canvas not found in the scene!");
+        }
+#endif
+
     }
 
     [Header("Detail Panel UI")]
@@ -48,15 +64,18 @@ public class ShopManager : MonoBehaviour
 
     private string _selectedMintId;
 
+        private Coroutine _loadCoroutine;
+
     void HidePopUp()
     {
         UIManager.Instance.SelectDefaultFeatureWindowOption();
     }
-    void GetAllShopData(bool sucess, GameShop response)
+
+    void GetAllShopData(bool success, GameShop response)
     {
-        if (sucess)
+        if (success)
         {
-            if ((_gameShop!=null)&& _gameShop.boosters.speed_boosters.speed_booster_6.price.SOL == response.boosters.speed_boosters.speed_booster_6.price.SOL &&
+            if ((_gameShop != null) && _gameShop.boosters.speed_boosters.speed_booster_6.price.SOL == response.boosters.speed_boosters.speed_booster_6.price.SOL &&
                 _gameShop.boosters.double_jump_boosters.double_jump_3.price.SOL == response.boosters.double_jump_boosters.double_jump_3.price.SOL &&
                 _gameShop.skins.alienSkin.price.SOL == response.skins.alienSkin.price.SOL)
             {
@@ -64,9 +83,6 @@ public class ShopManager : MonoBehaviour
                 return;
             }
             _gameShop = response;
-            //StaticDataBank.SpeedBoosterCollectionID = _gameShop.boosters.speed_boosters.speed_booster_3.collectionId;
-            //StaticDataBank.DoubleJumpCollectionID = _gameShop.boosters.double_jump_boosters.double_jump_3.collectionId;
-            //StaticDataBank.SkinCollectionID = _gameShop.skins.alienSkin.collectionId;
             StartCoroutine(PopulateData());
         }
         else
@@ -74,21 +90,7 @@ public class ShopManager : MonoBehaviour
             UIManager.Instance.ActivateFailureScreen("shop");
         }
     }
-    //Sprite DownloadImage(string imageUrl)
-    //{
-    //    Sprite get_sprite = null;
-    //    API_Manager.Instance.DownloadImage(imageUrl, (success, m_sprite) => {
-    //        if (success)
-    //        {
-    //            m_sprite = get_sprite;
-    //        }
-    //        else
-    //        {
-    //            Debug.Log("Failed to fetch image");
-    //        }
-    //    });
-    //    return get_sprite;
-    //}
+
     IEnumerator PopulateData()
     {
         StartCoroutine(DownloadImage(_gameShop.boosters.double_jump_boosters.double_jump_3.imageUrl, _gameShop.boosters.double_jump_boosters.double_jump_3.name, _gameShop.boosters.double_jump_boosters.double_jump_3.price.SOL, _gameShop.boosters.double_jump_boosters.double_jump_3.price.USDC, _gameShop.boosters.double_jump_boosters.double_jump_3.attributes[0].value, doublejump[0]));
@@ -112,17 +114,17 @@ public class ShopManager : MonoBehaviour
         yield return new WaitForSecondsRealtime(1f);
         UIManager.Instance.SelectDefaultFeatureWindowOption();
     }
-    IEnumerator DownloadImage(string imageUrl, string imageName, string SOLPrice, string USDCPrice, string _UsesLeft,UIDataConatainer data)
+
+    IEnumerator DownloadImage(string imageUrl, string imageName, string SOLPrice, string USDCPrice, string _UsesLeft, UIDataConatainer data)
     {
         bool istartchecking = false;
-        GlobalFeaturesManager.Instance.ImageCache.DownloadImage(imageUrl, imageName,(m_sprite) => {
-            
+        GlobalFeaturesManager.Instance.ImageCache.DownloadImage(imageUrl, imageName, (m_sprite) =>
+        {
             istartchecking = true;
-            
             if (m_sprite != null)
             {
                 Sprite tempSprite = Sprite.Create(m_sprite, new Rect(0, 0, m_sprite.width, m_sprite.height), new Vector2(0.5f, 0.5f));
-                
+
                 if (spriteDictionary.ContainsKey(imageName))
                 {
                     spriteDictionary[imageName] = tempSprite;
@@ -136,8 +138,8 @@ public class ShopManager : MonoBehaviour
                 data.SOL_Price.text = SOLPrice;
                 data.USDC_Price.text = USDCPrice;
                 data.UsesLeft.text = _UsesLeft;
-                
-                if(imageName.Contains("Skin"))
+
+                if (imageName.Contains("Skin"))
                     data.boosterImage.SetNativeSize();
             }
             else
@@ -145,10 +147,10 @@ public class ShopManager : MonoBehaviour
                 Debug.Log("Failed to fetch image");
             }
         });
-        
+
         yield return new WaitUntil(() => istartchecking);
     }
-    
+
     public void GetSkinsDetails(int index)
     {
         switch (index)
@@ -195,7 +197,7 @@ public class ShopManager : MonoBehaviour
                 break;
         }
     }
-    
+
     public void GetSpeedBoostersDetails(int index)
     {
         switch (index)
@@ -219,29 +221,6 @@ public class ShopManager : MonoBehaviour
 
         }
     }
-    public void GetJumpBoostersDetails(int index)
-    {
-        switch (index)
-        {
-            case 0:
-                GameShop.Booster jump = _gameShop.boosters.double_jump_boosters.double_jump_3;
-                ShowDetailPanel(jump, "double_jump_3");
-                break;
-            case 1:
-                GameShop.Booster jump1 = _gameShop.boosters.double_jump_boosters.double_jump_6;
-                ShowDetailPanel(jump1, "double_jump_6");
-                break;
-            case 2:
-                GameShop.Booster jump2 = _gameShop.boosters.double_jump_boosters.double_jump_10;
-                ShowDetailPanel(jump2, "double_jump_10");
-                break;
-            case 3:
-                GameShop.Booster jump3 = _gameShop.boosters.double_jump_boosters.double_jump_999;
-                ShowDetailPanel(jump3, "double_jump_999");
-                break;
-
-        }
-    }
 
     public void ShowDetailPanel(GameShop.Skin skin)
     {
@@ -253,26 +232,18 @@ public class ShopManager : MonoBehaviour
         attribute.text = skin.attributes[0].traitType + ":" + skin.attributes[0].value;
         _selectedMintId = skin.attributes[0].value;
         DetailsBuyButtonPressed();
-
-        //MintButton.gameObject.SetActive(true);
-        //ListButton.gameObject.SetActive(false);
     }
+
     public void ShowDetailPanel(GameShop.Booster booster, string mintid)
     {
         if (spriteDictionary.ContainsKey(booster.name))
             boosterImage.sprite = spriteDictionary[booster.name];
-        
         boosterImage.SetNativeSize();
-        
-        //BoosterName.text = StaticDataBank.RemoveWordFromString(booster.name);
         BoosterName.text = booster.name;
         Description.text = booster.description;
         attribute.text = booster.attributes[0].traitType + ":" + booster.attributes[0].value;
         _selectedMintId = mintid;
         DetailsBuyButtonPressed();
-
-        //MintButton.gameObject.SetActive(true);
-        //ListButton.gameObject.SetActive(false);
     }
 
     public void DetailsBuyButtonPressed()
@@ -288,31 +259,39 @@ public class ShopManager : MonoBehaviour
         newPopupData.contentString = "Choose a currency to buy with";
         newPopupData.firstButtonString = "SOL";
         newPopupData.secondButtonString = "USD";
-        newPopupData.firstButtonCallBack = () => MintNft(MintID,true);
-        newPopupData.secondButtonCallBack = () => MintNft(MintID,false);
+        newPopupData.firstButtonCallBack = () => MintNft(MintID, true);
+        newPopupData.secondButtonCallBack = () => MintNft(MintID, false);
         GlobalCanvasManager.Instance.PopUIHandler.ShowPopup(newPopupData);
         GlobalCanvasManager.Instance.PopUIHandler.ToggleSpecialKillButton(true);
-        
     }
-    public void MintNft(string itemName,bool withSol)
+
+    private void OnDisable()
+    {
+        if (_loadCoroutine != null)
+        {
+            StopCoroutine(_loadCoroutine);
+        }
+    }
+    public void MintNft(string itemName, bool withSol)
     {
         GlobalCanvasManager.Instance.LoadingPanel.ShowPopup("Processing payment", 5,
-    new List<SocketEventsType> { SocketEventsType.paymentComplete });
-        
+            new List<SocketEventsType> { SocketEventsType.paymentComplete });
+
         Debug.Log("Item Name : " + itemName);
-        API_Manager.Instance.BuyNft(itemName,withSol, (success, message) =>
+        API_Manager.Instance.BuyNft(itemName, withSol, (success, message) =>
         {
             if (success)
             {
-                Utils.OpenURLInNewTab(message);
-                // Output the URL
+                // Open URL in WebView
+                _loadCoroutine = StartCoroutine(LoadWebView(message));
+                webViewObject.SetVisibility(true);
                 Debug.Log("Checkout URL: " + message);
             }
             else
             {
                 Debug.Log(message);
                 GlobalCanvasManager.Instance.LoadingPanel.HidePopup();
-                
+
                 GlobalCanvasManager.Instance.PopUIHandler.ShowPopup(new PopupData()
                 {
                     titleString = "Error",
@@ -320,8 +299,73 @@ public class ShopManager : MonoBehaviour
                     firstButtonString = "OK",
                     firstButtonCallBack = null
                 });
-            } 
+            }
         });
+    }
+
+    private void OpenURLInWebView(string Url)
+    {
+#if !UNITY_WEBPLAYER && !UNITY_WEBGL
+        if (Url.StartsWith("http"))
+        {
+            webViewObject.LoadURL(Url.Replace(" ", "%20"));
+        }
+        else
+        {
+            var exts = new string[]{
+                ".jpg",
+                ".js",
+                ".html"  // should be last
+            };
+            foreach (var ext in exts)
+            {
+                var url = Url.Replace(".html", ext);
+                var src = System.IO.Path.Combine(Application.streamingAssetsPath, url);
+                var dst = System.IO.Path.Combine(Application.temporaryCachePath, url);
+                byte[] result = null;
+                if (src.Contains("://"))
+                {  // for Android
+#if UNITY_2018_4_OR_NEWER
+                    // NOTE: a more complete code that utilizes UnityWebRequest can be found in https://github.com/gree/unity-webview/commit/2a07e82f760a8495aa3a77a23453f384869caba7#diff-4379160fa4c2a287f414c07eb10ee36d
+                    var unityWebRequest = UnityWebRequest.Get(src);
+                    yield return unityWebRequest.SendWebRequest();
+                    result = unityWebRequest.downloadHandler.data;
+#else
+                    var www = new WWW(src);
+                    yield return www;
+                    result = www.bytes;
+#endif
+                }
+                else
+                {
+                    result = System.IO.File.ReadAllBytes(src);
+                }
+                System.IO.File.WriteAllBytes(dst, result);
+                if (ext == ".html")
+                {
+                    webViewObject.LoadURL("file://" + dst.Replace(" ", "%20"));
+                    break;
+                }
+            }
+        }
+#else
+        if (Url.StartsWith("http")) {
+            webViewObject.LoadURL(Url.Replace(" ", "%20"));
+        } else {
+            webViewObject.LoadURL("StreamingAssets/" + Url.Replace(" ", "%20"));
+        }
+#endif
+        // var webView = gameObject.AddComponent<WebViewObject>();
+        // webViewObject.LoadURL(url);
+        // webView.OnLoaded += (sender, args) =>
+        // {
+        //     Debug.Log("WebView Loaded");
+        // };
+        // webView.OnError += (sender, args) =>
+        // {
+        //     Debug.LogError("WebView Error: " + args.message);
+        // };
+        // webView.Load();
     }
 
     public void ReactivateDetailPanel()
@@ -332,5 +376,173 @@ public class ShopManager : MonoBehaviour
     public void FetchShopData()
     {
         API_Manager.Instance.GetShopData(GetAllShopData);
+    }
+
+    // Note: Load web view loads the page but wont make it visible.
+    // to do this, you must run SetVisibility(true);
+    private IEnumerator LoadWebView(string Url)
+    {
+        
+        webViewObject.Init(
+            cb: (msg) =>
+            {
+                // Debug.Log(string.Format("CallFromJS[{0}]", msg));
+            },
+            err: (msg) =>
+            {
+                // Debug.Log(string.Format("CallOnError[{0}]", msg));
+            },
+            httpErr: (msg) =>
+            {
+                // Debug.Log(string.Format("CallOnHttpError[{0}]", msg));
+            },
+            started: (msg) =>
+            {
+                // Debug.Log(string.Format("CallOnStarted[{0}]", msg));
+            },
+            hooked: (msg) =>
+            {
+                // Debug.Log(string.Format("CallOnHooked[{0}]", msg));
+            },
+            cookies: (msg) =>
+            {
+                // Debug.Log(string.Format("CallOnCookies[{0}]", msg));
+            },
+            ld: (msg) =>
+            {
+                // Debug.Log(string.Format("CallOnLoaded[{0}]", msg));
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX || UNITY_IOS
+                // NOTE: the following js definition is required only for UIWebView; if
+                // enabledWKWebView is true and runtime has WKWebView, Unity.call is defined
+                // directly by the native plugin.
+#if true
+                var js = @"
+                    if (!(window.webkit && window.webkit.messageHandlers)) {
+                        window.Unity = {
+                            call: function(msg) {
+                                window.location = 'unity:' + msg;
+                            }
+                        };
+                    }
+                ";
+#else
+                // NOTE: depending on the situation, you might prefer this 'iframe' approach.
+                // cf. https://github.com/gree/unity-webview/issues/189
+                var js = @"
+                    if (!(window.webkit && window.webkit.messageHandlers)) {
+                        window.Unity = {
+                            call: function(msg) {
+                                var iframe = document.createElement('IFRAME');
+                                iframe.setAttribute('src', 'unity:' + msg);
+                                document.documentElement.appendChild(iframe);
+                                iframe.parentNode.removeChild(iframe);
+                                iframe = null;
+                            }
+                        };
+                    }
+                ";
+#endif
+#elif UNITY_WEBPLAYER || UNITY_WEBGL
+                var js = @"
+                    window.Unity = {
+                        call:function(msg) {
+                            parent.unityWebView.sendMessage('WebViewObject', msg);
+                        }
+                    };
+                ";
+#else
+                var js = "";
+#endif
+                webViewObject.EvaluateJS(js + @"Unity.call('ua=' + navigator.userAgent)");
+            }
+            //transparent: false,
+            //zoom: true,
+            //ua: "custom user agent string",
+            //radius: 0,  // rounded corner radius in pixel
+            //// android
+            //androidForceDarkMode: 0,  // 0: follow system setting, 1: force dark off, 2: force dark on
+            //// ios
+            //enableWKWebView: true,
+            //wkContentMode: 0,  // 0: recommended, 1: mobile, 2: desktop
+            //wkAllowsLinkPreview: true,
+            //// editor
+            //separated: false
+            );
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+        webViewObject.bitmapRefreshCycle = 1;
+        webViewObject.devicePixelRatio = 1;  // 1 or 2
+#endif
+        // cf. https://github.com/gree/unity-webview/pull/512
+        // Added alertDialogEnabled flag to enable/disable alert/confirm/prompt dialogs. by KojiNakamaru · Pull Request #512 · gree/unity-webview
+        //webViewObject.SetAlertDialogEnabled(false);
+
+        // cf. https://github.com/gree/unity-webview/pull/728
+        //webViewObject.SetCameraAccess(true);
+        //webViewObject.SetMicrophoneAccess(true);
+
+        // cf. https://github.com/gree/unity-webview/pull/550
+        // introduced SetURLPattern(..., hookPattern). by KojiNakamaru · Pull Request #550 · gree/unity-webview
+        //webViewObject.SetURLPattern("", "^https://.*youtube.com", "^https://.*google.com");
+
+        // cf. https://github.com/gree/unity-webview/pull/570
+        // Add BASIC authentication feature (Android and iOS with WKWebView only) by takeh1k0 · Pull Request #570 · gree/unity-webview
+        //webViewObject.SetBasicAuthInfo("id", "password");
+
+        //webViewObject.SetScrollbarsVisibility(true);
+
+        webViewObject.SetMargins(12, 12, 12, 12);
+        webViewObject.SetTextZoom(100);  // android only. cf. https://stackoverflow.com/questions/21647641/android-webview-set-font-size-system-default/47017410#47017410
+
+#if !UNITY_WEBPLAYER && !UNITY_WEBGL
+        if (Url.StartsWith("http"))
+        {
+            webViewObject.LoadURL(Url.Replace(" ", "%20"));
+        }
+        else
+        {
+            var exts = new string[]{
+                ".jpg",
+                ".js",
+                ".html"  // should be last
+            };
+            foreach (var ext in exts)
+            {
+                var url = Url.Replace(".html", ext);
+                var src = System.IO.Path.Combine(Application.streamingAssetsPath, url);
+                var dst = System.IO.Path.Combine(Application.temporaryCachePath, url);
+                byte[] result = null;
+                if (src.Contains("://"))
+                {  // for Android
+#if UNITY_2018_4_OR_NEWER
+                    // NOTE: a more complete code that utilizes UnityWebRequest can be found in https://github.com/gree/unity-webview/commit/2a07e82f760a8495aa3a77a23453f384869caba7#diff-4379160fa4c2a287f414c07eb10ee36d
+                    var unityWebRequest = UnityWebRequest.Get(src);
+                    yield return unityWebRequest.SendWebRequest();
+                    result = unityWebRequest.downloadHandler.data;
+#else
+                    var www = new WWW(src);
+                    yield return www;
+                    result = www.bytes;
+#endif
+                }
+                else
+                {
+                    result = System.IO.File.ReadAllBytes(src);
+                }
+                System.IO.File.WriteAllBytes(dst, result);
+                if (ext == ".html")
+                {
+                    webViewObject.LoadURL("file://" + dst.Replace(" ", "%20"));
+                    break;
+                }
+            }
+        }
+#else
+        if (Url.StartsWith("http")) {
+            webViewObject.LoadURL(Url.Replace(" ", "%20"));
+        } else {
+            webViewObject.LoadURL("StreamingAssets/" + Url.Replace(" ", "%20"));
+        }
+#endif
+        yield break;
     }
 }
